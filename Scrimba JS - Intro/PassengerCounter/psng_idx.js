9e205774129a6393814a2c2f6f411a87242ad1de
Craig Rupp <craigrupp@utexas.edu>
2022-06-01 20:59:36 -0500

    Quick counter app finished

--- Scrimba JS - Intro/PassengerCounter/psng_idx.js
+++ Scrimba JS - Intro/PassengerCounter/psng_idx.js
@@ -1,8 +1,23 @@
-// document.getElementById("count-el").innerText = 25;
-let bonusPoints = 50;
-bonusPoints += 50
-console.log(bonusPoints)
-bonusPoints -= 75
-console.log(bonusPoints)
-bonusPoints += 45
-console.log(bonusPoints)
+
+
+let count = 0
+
+function increment(){
+    count += 1
+    document.getElementById('count-el').textContent = count
+    console.log(count)
+}
+
+function save(){
+    //alert('The final count is : ' + count.toString())
+    document.getElementById('save-el').textContent += count.toString() + " - "
+    console.log('The final count is : ' + count.toString())
+    document.getElementById('count-el') = 0
+    count = 0   
+}
+
+
+console.log(4 + 5) // 9
+console.log("2" + "4") // "24"
+console.log("5" + 1) // "51"
+console.log(100 + "100") // "100100"
