93a2b8e913942289b25b2132af1f3898dea5588d
Craig Rupp <craigrupp@utexas.edu>
2022-06-01 19:22:29 -0500

    Initial working css/html template for passenger counter

---
+++ Scrimba JS - Intro/PassengerCounter/psng_idx.js
@@ -0,0 +1,8 @@
+// document.getElementById("count-el").innerText = 25;
+let bonusPoints = 50;
+bonusPoints += 50
+console.log(bonusPoints)
+bonusPoints -= 75
+console.log(bonusPoints)
+bonusPoints += 45
+console.log(bonusPoints)
