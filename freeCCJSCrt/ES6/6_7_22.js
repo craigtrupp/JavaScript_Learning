e3cbbbe02f353f545908e14133ba4bdb6e9963ab
Craig Rupp <craigrupp@utexas.edu>
2022-06-07 13:20:23 -0500

    ES6 Folder first Challenged Add

---
+++ freeCCJSCrt/ES6/6_7_22.js
@@ -0,0 +1,34 @@
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+  
+checkScope()
+
+// Block scope i is:  block scope
+// Function scope i is:  function scope
+
+
+// In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. 
+// You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS)
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
