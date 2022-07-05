affd51ccc6fe3b2bef7dd778d81a8ac167dc1d35
Craig Rupp <craigrupp@utexas.edu>
2022-07-04 19:29:44 -0500

    Few more scripting challenges

--- freeCCJSCrt/DataStructures/basascript.js
+++ freeCCJSCrt/DataStructures/basascript.js
@@ -32,4 +32,42 @@ function findLongestWordLength(str) {
   }
   
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+function findLongestWordLength_deux(s) {
+    return s.split(' ')
+      .reduce(function(longest, word) {
+        return Math.max(longest, word.length)
+      }, 0);
+}
+//In case you’re wondering about that 0 after the callback function, it is used to give an initial value to the longest, so that Math.max will know where to start.
+
+
+// Return Largest Numbers in Arrays
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+function largestOfFour(arr) {
+  let largest_set = [];
+  for(let i = 0; i < arr.length; i++){
+   largest_set.push(Math.max(...arr[i]))
+   console.log(arr[i]);
+  }
+  return largest_set;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+// Confirm the Ending
+// Check if a string (first argument, str) ends with the given target string (second argument, target). : Confirm the Ending
+function confirmEnding(str, target){
+  let iterations = str.length - target.length;
+  let count_return = [];
+  for(let i = str.length - 1; i >= iterations; i--){
+    count_return.push(str[i]);
+    console.log(str[i]);
+  }
+  let target_string = count_return.reverse().join("");
+  return target_string == target ? true : false;
+}
+console.log(confirmEnding("Open sesame", "sage"))
   
