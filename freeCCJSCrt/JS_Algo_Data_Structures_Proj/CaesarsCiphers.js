e58e673d0bb9cb31f01efca523e0a591dc9cdbb5
Craig Rupp <craigrupp@utexas.edu>
2022-08-02 15:45:29 -0500

    Caesar's Cipher

--- freeCCJSCrt/JS_Algo_Data_Structures_Proj/CaesarsCiphers.js
+++ freeCCJSCrt/JS_Algo_Data_Structures_Proj/CaesarsCiphers.js
@@ -8,12 +8,31 @@
 // All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 
 function rot13(str) {
-    //let alphabet = [{'a':14, 'b':15, 'c':16, 'd':17, 'e':18, 'f':19, 'g':20, 'h':21, 'i':22, 'j': 23, 'k': 24, 'l': 25, 'm':26, 'n':13}];
+    let characters = [];
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    //alphabet.indexOf(str[0]) < 13 (alphabet.indexOf(str[i] + 13)) count backwards if .indexOf(str[i]) = iterator in loop ; (if(str[i].test(/alphabet only/) {if}))
-    //nest this first alpha regex if(str[i] < 13) {let lookforward = alphabet.indexOf(str[i]) + 13; new_str.concat(alphabet[lookforward])} break;
-    // else (non-alpha) {new_str.concat(str[i])}
-    return str;
+    for(let i = 0; i < str.length; i++){
+      if(/\w/i.test(str[i])){
+        let index = alphabet.indexOf(str[i].toLowerCase())
+        if(index < 13){
+          characters.push(alphabet[index + 13]);
+        }
+        else{
+          characters.push(alphabet[index - 13]);
+        }
+      } else {
+        characters.push(str[i]);
+      }
+    }
+    return characters.join('').toUpperCase();
   }
   
-  rot13("SERR PBQR PNZC"); // "FREE CODE CAMP"
+  console.log(rot13("SERR PBQR PNZC"));
+  console.log(rot13("SERR CVMMN!"));
+  console.log(rot13("SERR YBIR?"));
+  console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
+
+  
+// FREE CODE CAMP
+// FREE PIZZA!
+// FREE LOVE?
+// THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
