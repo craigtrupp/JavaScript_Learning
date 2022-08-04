4912c155d0b876cfaa48c5731626deb3160638ca
Craig Rupp <craigrupp@utexas.edu>
2022-08-04 12:19:59 -0500

    Ok .. reshuffle, got rid of decimals and now have total from provided CID drawer in function, three return type logic needed (a bit similar to roman numerals) to see if value can be returned in highest to lowest order

--- freeCCJSCrt/JS_Algo_Data_Structures_Proj/CashRegister.js
+++ freeCCJSCrt/JS_Algo_Data_Structures_Proj/CashRegister.js
@@ -35,36 +35,37 @@
 //   ["ONE HUNDRED", 100]
 // ]
 
-// function checkCashRegister(price, cash, cid) {
-//     let difference = cash - price;
-//     let currency_values= {'PENNY': 1, 'NICKEL':5, 'DIME': .1, 'QUARTER':.25, 'ONE':1, 'FIVE':5, 'TEN':10, 'TWENTY':20, 'ONE HUNDRED':100}
-//     let running_change = {'difference': [difference], 'change_tendered':[0]}
-//     let changeTotal = function(){
-//         return running_change['change_tendered'].reduce((partialSum, a) => partialSum + a);
-//     }
-//     function getChange(target){
-//         for(let i = cid.length-1; i >= 0; i--){
-//             for (const[key, value] of Object.entries(currency_values)){
-//                 if(value <= target){
-//                     running_change['difference'].unshift(running_change.difference[0] - value);
-//                     running_change['change_tendered'].push(value);
-//                     return;
-//                 }
-//             }
-//         }
-//     }
-//     while(changeTotal() != difference){
-//         getChance(running_change['difference'][0]);
-//     }
-//     return running_change;
-//   }
+
+function checkCashRegister(price, cash, cid) {
+    let currency_values= {'PENNY': 1, 'NICKEL':5, 'DIME': 10, 'QUARTER':25, 'ONE':100, 'FIVE':500, 'TEN':1000, 'TWENTY':2000, 'ONE HUNDRED':10000}
+    // Floats are the worst ... let's not deal with them
+    let difference = (cash * 100) - (price * 100);
+    // this value will be set after the forEach runs below to calculate total sum of all actual cash in Drawer (3rd) argument
+    let cidSum = 0;
+    // make sure CID is available for all 2D  "nickel" .. etc type elements, else filter out currency 'key'
+    let actualCID = cid.forEach(element => element[1] != 0).reverse();
+    actualCID.forEach(element => {
+        let currency = element[0];
+        let currencySum = element[1] * 100;
+        cidSum += currencySum;
+    });
+  }
+  
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-  //should return {status: "OPEN", change: [["QUARTER", 0.5]]}
+  //should return {status: "OPEN", change: [["QUARTER", 0.5]]} (Current Iteration Below)
+  // [ [ [ 'PENNY', 1.01 ],
+//     [ 'NICKEL', 2.05 ],
+//     [ 'DIME', 3.1 ],
+//     [ 'QUARTER', 4.25 ],
+//     [ 'ONE', 90 ],
+//     [ 'FIVE', 55 ],
+//     [ 'TEN', 20 ],
+//     [ 'TWENTY', 60 ],
+//     [ 'ONE HUNDRED', 100 ] ],
+//   33541,
+//   50 ]
   checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
   //{status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
   
   
-  // while(change() < cash - price){change()}
-  // cash - price (difference aka change required)
-  // {'PENNY' : 01, 'NICKEL' : 05}
